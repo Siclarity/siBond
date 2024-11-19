33416ebcfcffb3e9983f26b286600ce2b20079f2
Si-Aneal-Kuverji <aneal@siclarity.com>
2024-11-18 16:39:14 -0800

    KN43-store files in the backend and started making bondSearch file for tying into functionality. Currently researching on how to get the layout of the uploaded GDS file on the FLASK page.

--- website/app.py
+++ website/app.py
@@ -7,7 +7,7 @@ from os.path import isfile, join
 app = Flask(__name__)
 
 Uploaded_GDS='uploads'
-app.config['Uploaded_GDS'] =Uploaded_GDS 
+app.config['Uploaded_GDS']=Uploaded_GDS 
 os.makedirs(Uploaded_GDS, exist_ok=True)
 
 @app.route("/")
@@ -22,32 +22,41 @@ def getFile():
     if request.method=='GET':
        return jsonify({"success": True})
     if request.method == 'POST':
-        print("request:",request.get_json())
-        data=request.get_json()
-        print(data)
-        if 'file' not in data:
+        # print("request:",request.get_json())
+        # data=request.get_json()
+        # print(data)
+        # if 'file' not in data:
+        if 'file' not in request.files:
             #return jsonify({'error': 'No file part'})
-            print("Error: No file part") 
-        file=data['file']
+            print("Error: No file part"),400
+        #file=data['file']
+        file=request.files['file']
         #if there is no name for the file
         print(file)
-        filename=file.split('\\')[-1]
-        print(filename)
-        if filename == '':
+        #filename=file.split('\\')[-1]
+        print(file.filename)
+        if file.filename == '':
             #return jsonify({'error':'No selected file'})
             print("Error:There is no file selected")
+            return jsonify({'error':'No selected file'}),400
         #if there is a file
+        filename=file.filename
         if filename:
             #set the filename variable equal to the name of the file
             #ilename = file
             filepath = os.path.join(app.config['Uploaded_GDS'], filename)
             #runs into error here with the file save as file is a string and needs to be a file not string
-            file.save(filepath)
-            #return jsonify({'success':'File uploaded successfully'})
-            print("Successfully uploaded:",filename)
+            try:
+                file.save(filepath)
+                #return jsonify({'success':'File uploaded successfully'})
+                print("Successfully uploaded:",filename)
+                return jsonify({"success": "File uploaded successfully","filename":filename})
+            except Exception as e:
+                print("Error while saving file:", e)
+                return jsonify({"error": "File upload failed"}), 500
             #run bondsearch here
     #return render_template('gds.html')
-        return jsonify({"success": False})
+        #return jsonify({"success": False})
 @app.route('/meshGenerator')
 def meshGenerator():
 
